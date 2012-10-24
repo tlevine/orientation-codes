8836f7c2a450e86a96e4f41e52a8d28295f7660b
Thomas Levine <occurrence@thomaslevine.com>
2012-10-24 14:52:22 -0400

    adjust things

--- lib.py
+++ lib.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
+import re
 
 def convert(oldcode):
     'Convert the old code to the new code.'
-    return 0
+    if '+' in oldcode:
+        beforeplus, afterplus = oldcode.split('+')
+    else:
+        beforeplus = oldcode
+
+    z, x, q = [int(re.sub(r'[^0-9]', '',thing)) for thing in  oldcode.split('.')]
+
+    rightsideup = z == 5
+
+    if q == 0:
+        angle = x
+    elif q ==1:
+        angle = x
+    elif q == 2:
+        angle = 180 - x
+    elif q == 3:
+        angle = 270
+
+    return (angle, rightsideup)
