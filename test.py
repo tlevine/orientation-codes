deda150c7aab11edea71ee14b4e0c9502359144b
Thomas Levine <occurrence@thomaslevine.com>
2012-10-24 14:34:44 -0400

    more tests

--- test.py
+++ test.py
@@ -1,5 +1,31 @@
 import nose.tools as n
+import lib
 
 class TestConversion:
-    def test_foo(self):
-        n.assert_true(True)
+    def _(self, orig, expected):
+        observed = lib.convert(orig)
+        n.assert_equal(observed, expected)
+
+    def test1(self):
+        self._('Z5.x05.q0+L', 85)
+
+    def test_calculator(self):
+        self._('z5.x90.q0', 90)
+
+    def test_s3_calculator_best(self):
+        self._('z5.x50.q0+L', 50)
+
+    def test_s4_car_best(self):
+        self._('z5.x05.q0+l', )
+
+    def test1(self):
+        self._('', )
+
+    def test1(self):
+        self._('', )
+
+    def test1(self):
+        self._('', )
+
+    def test1(self):
+        self._('', )
